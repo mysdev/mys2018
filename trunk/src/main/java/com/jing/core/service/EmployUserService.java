2e410cc6d5702cbf4cd4110ea342683642abdf7e
lifengatgithub <lifengat@foxmail.com>
2018-01-17 17:23:23 +0800

    员工to用户

---
+++ trunk/src/main/java/com/jing/core/service/EmployUserService.java
@@ -0,0 +1,47 @@
+package com.jing.core.service;
+
+import com.jing.core.model.entity.Employee;
+import com.jing.system.model.entity.User;
+
+/**
+ * 员工+用户
+ * @author Administrator
+ *
+ */
+public interface EmployUserService {
+
+	/**
+	 * 添加员工 + 用户
+	 * 1.新增user
+	 * 2.赋值userId到employee
+	 * 3.新增employee
+	 */
+	void addEmployee(Employee employee,User user);
+	
+	/**
+	 * 员工转用户
+	 * 1.查询employee
+	 * 2.新增user
+	 * 3.修改employee的userId
+	 * @param employee
+	 * @param user
+	 */
+	void employeeToUser(String empId,User user);
+	
+	/**
+	 * 修改员工 + 用户
+	 * 1.update user
+	 * 2.update employee
+	 */
+	void updateEmployee(Employee employee,User user);
+	
+	/**
+	 * 删除员工 + 用户
+	 * 1.查询员工
+	 * 2.逻辑删除emplyee
+	 * 3.修改user的status=1
+	 */
+	void deleteEmployee(String empId);
+	
+	
+}
---
+++ trunk/src/main/java/com/jing/core/service/bo/EmployeeBo.java
@@ -0,0 +1,13 @@
+package com.jing.core.service.bo;
+
+import com.jing.core.model.entity.Employee;
+
+/**
+ * 员工
+ * @author Administrator
+ *
+ */
+public class EmployeeBo extends Employee{
+	private static final long serialVersionUID = 1L;
+
+}
