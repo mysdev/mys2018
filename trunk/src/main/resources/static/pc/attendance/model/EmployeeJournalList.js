f668ac8d2edd904a0a16a3244c202b552c466adc
Eddie008 <32446708+Eddie008@users.noreply.github.com>
2018-02-18 16:27:45 +0800

    Update EmployeeJournalList.js
    考勤页面修改

--- trunk/src/main/resources/static/pc/attendance/model/EmployeeJournalList.js
+++ trunk/src/main/resources/static/pc/attendance/model/EmployeeJournalList.js
@@ -10,7 +10,11 @@ if(getQueryString('page')!=undefined){
 function Node(obj) {
 	this.journalId = ko.observable(obj.journalId); 
 	this.empId = ko.observable(obj.empId); 
-	this.attendanceDate = ko.observable(obj.attendanceDate); 
+	this.empName = ko.observable(obj.empName); 
+	this.attendance = ko.observable(obj.attendance);
+	this.attTime = ko.observable(obj.attTime);
+	this.sysSign = ko.observable(obj.sysSign); 
+	this.sysOut = ko.observable(obj.sysOut); 
 	this.signTime = ko.observable(obj.signTime); 
 	this.outTime = ko.observable(obj.outTime); 
 	this.createdBy = ko.observable(obj.createdBy); 
@@ -28,15 +32,11 @@ var EmployeeJournalViewModel = function () {
     
     //重载数据
     self.reloadData = function(){
-    	myAjax("/employeejournals", "GET", query, function (data){
+    	myAjax("/attendance/employee/diarys", "GET", query, function (data){
 			var mappedTasks = $.map(data.data, function(item) { return new Node(item) });  
 			self.employeeJournalList(mappedTasks);
 			myPage = data.page;
 			bindPage();
-			    
-			$("table tbody td .tomodify").bind(function(){
-				ChangeUrl('./attendance/EmployeeJournal.html?action=Edit&id='+$(this).attr('data'));
-			});
 		}, true);
     };
     
@@ -56,52 +56,6 @@ var EmployeeJournalViewModel = function () {
     self.add = function(obj) {
     	ChangeUrl('./attendance/EmployeeJournal.html?action=Add');
     };
-    
-    //修改
-    self.modify=function(obj){
-    	ChangeUrl('./attendance/EmployeeJournal.html?action=Edit&id='+obj.journalId());
-    };
-    
-    //删除
-    self.delete=function(obj){
-    	parent.dialog({
-            title: '提示',
-            content: '确定要删除该记录！',
-            okValue: '确定',
-            ok: function () {
-		    	var id = $(event.currentTarget).attr('data');
-		    	myAjax("/employeejournal/"+id, "DELETE", null, reloadDate, false);
-			}
-        }).showModal();
-    }
-    
-    //批量删除
-    self.deletes = function(obj) {
-    	if ($(".checkall input:checked").size() < 1) {
-        	parent.dialog({
-	            title: '提示',
-	            content: '对不起，请选中您要操作的记录！',
-	            okValue: '确定',
-	            ok: function () { }
-	        }).showModal();
-	        return false;
-	    }
-	    var msg = "删除记录后不可恢复，您确定吗？";
-	    parent.dialog({
-	        title: '提示',
-	        content: msg,
-	        okValue: '确定',
-	        ok: function () {
-	        	$(".checkall input:checked").each(function(i){
-	        		myAjax("/employeejournal/"+id, "DELETE", null, null, false);
-	        	});
-	        	location.reload();
-	        },
-	        cancelValue: '取消',
-	        cancel: function () { }
-	    }).showModal();
-    };
-
 };
 
 $().ready(function(){
