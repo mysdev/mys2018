afeacf16c6da522d52f8315f5c4e50edd61911aa
Eddie008 <32446708+Eddie008@users.noreply.github.com>
2018-02-18 16:24:44 +0800

    Update EmployeeAttendanceList.js
    考勤页面修改

--- trunk/src/main/resources/static/pc/attendance/model/EmployeeAttendanceList.js
+++ trunk/src/main/resources/static/pc/attendance/model/EmployeeAttendanceList.js
@@ -1,3 +1,5 @@
+var bIsInit = false;
+var attendanceList=[];
 var myPage;
 var viewMode;
 var query = {};
@@ -10,7 +12,9 @@ if(getQueryString('page')!=undefined){
 function Node(obj) {
 	this.linkId = ko.observable(obj.linkId); 
 	this.empId = ko.observable(obj.empId); 
+	this.empName = ko.observable(obj.empName); 
 	this.attendanceId = ko.observable(obj.attendanceId); 
+	this.attendanceName = ko.observable(obj.attendanceName); 
 	this.createdBy = ko.observable(obj.createdBy); 
 	this.createdDate = ko.observable(obj.createdDate); 
 	this.updatedBy = ko.observable(obj.updatedBy); 
@@ -21,33 +25,32 @@ function Node(obj) {
 //定义ViewModel对象
 var EmployeeAttendanceViewModel = function () {  
 	self=this;
+	
+	self.attendanceList=ko.observableArray(attendanceList);
+    self.attendanceSelected = ko.observable('');
+    
     //添加动态监视数组对象
     self.employeeAttendanceList = ko.observableArray([]);
     
     //重载数据
     self.reloadData = function(){
-    	myAjax("/employeeattendances", "GET", query, function (data){
-			var mappedTasks = $.map(data.data, function(item) { return new Node(item) });  
-			self.employeeAttendanceList(mappedTasks);
-			myPage = data.page;
-			bindPage();
-			    
-			$("table tbody td .tomodify").bind(function(){
-				ChangeUrl('./attendance/EmployeeAttendance.html?action=Edit&id='+$(this).attr('data'));
-			});
-		}, true);
+    	if(self.attendanceSelected()=="") return; 
+    	
+    	GetData();
     };
     
     	
     //初始化数据--如何调用self.reloadData
     self.reloadData();
 	
+	self.attendanceChage=function(p){
+		if(bIsInit)
+    		self.reloadData();
+    };
+    
 	//搜索
 	self.search = function(obj) {
-		query.pageNo=1;
-		query.pageSize = 20;
-		qeury.keyWord = $("txtKeywords").val(); //查询参数格式
-		myAjax("/employeeattendances", "GET", query, doQueryActionSuccess, true);
+		GetData();
     };
     
     //新增
@@ -105,6 +108,21 @@ var EmployeeAttendanceViewModel = function () {
 $().ready(function(){
 	viewMode = new EmployeeAttendanceViewModel();
     ko.applyBindings(viewMode);
+    
+    //获取下拉列表数据
+	myAjax("/attendances", "GET", null, function (data){
+		$.map(data.data, function(item) { attendanceList.push({id:item.attendanceId,name:item.attendanceName})});  
+		viewMode.attendanceList(attendanceList);
+		viewMode.attendanceSelected(attendanceList[0].name);
+		$(".rule-single-select").ruleSingleSelect();
+		
+		bIsInit = true;
+		viewMode.reloadData();
+	}, true);
+	
+    //美化下拉框样式
+	$(".rule-single-select").ruleSingleSelect();
+	
 });
 
 var bindPage =function(){
@@ -125,3 +143,25 @@ var bindPage =function(){
     });
 }
 
+function GetData(){
+	var searchKey = $("#txtKeywords").val().toString(); 
+	var url = "/attendance/"+self.attendanceSelected()+"/employees";
+	
+	if(searchKey!="")
+		query.namePYJob=searchKey;
+	else
+		query.namePYJob='';
+		
+	myAjax(url, "GET", query, function (data){
+		var mappedTasks = $.map(data.data, function(item) { return new Node(item) });  
+		self.employeeAttendanceList(mappedTasks);
+		myPage = data.page;
+		
+		if(mappedTasks.length>0)
+			bindPage();
+		    
+		$("table tbody td .tomodify").bind(function(){
+			ChangeUrl('./attendance/EmployeeAttendance.html?action=Edit&id='+$(this).attr('data'));
+		});
+	}, true);
+}
