0f3967cf0c3b16a4307809dbb739cdb53e063097
Eddie008 <32446708+Eddie008@users.noreply.github.com>
2018-02-18 16:22:51 +0800

    Update AttendanceDetailList.js
    考勤页面修改

--- trunk/src/main/resources/static/pc/attendance/model/AttendanceDetailList.js
+++ trunk/src/main/resources/static/pc/attendance/model/AttendanceDetailList.js
@@ -1,19 +1,16 @@
-var myPage;
+var attendanceList=[];
 var viewMode;
-var query = {};
-query.pageNo=1;
-query.pageSize = 20;
-if(getQueryString('page')!=undefined){
-	query.pageNo=getQueryString('page');
-}
 
 function Node(obj) {
 	this.attId = ko.observable(obj.attId); 
-	this.attendanceId = ko.observable(obj.attendanceId); 
-	this.weekday = ko.observable(obj.weekday); 
-	this.attMonth = ko.observable(obj.attMonth); 
-	this.attDate = ko.observable(obj.attDate); 
+	this.attDay=ko.observable(obj.attDay); 
+	this.attDate = ko.observable(obj.attDate);
+	this.attMonth = ko.observable(obj.attMonth);
 	this.attendance = ko.observable(obj.attendance); 
+	this.attendanceId = ko.observable(obj.attendanceId); 
+	this.editable = ko.observable(obj.editable);
+	this.weekday = ko.observable(obj.weekday); 
+	this.timeId = ko.observable(obj.timeId);
 	this.createdBy = ko.observable(obj.createdBy); 
 	this.createdDate = ko.observable(obj.createdDate); 
 	this.updatedBy = ko.observable(obj.updatedBy); 
@@ -24,107 +21,70 @@ function Node(obj) {
 //定义ViewModel对象
 var AttendanceDetailViewModel = function () {  
 	self=this;
+	
+	self.attendanceList=ko.observableArray(attendanceList);
+    self.attendanceSelected = ko.observable('');
+    
     //添加动态监视数组对象
     self.attendanceDetailList = ko.observableArray([]);
     
     //重载数据
     self.reloadData = function(){
-    	myAjax("/attendancedetails", "GET", query, function (data){
-			var mappedTasks = $.map(data.data, function(item) { return new Node(item) });  
-			self.attendanceDetailList(mappedTasks);
-			myPage = data.page;
-			bindPage();
-			    
-			$("table tbody td .tomodify").bind(function(){
-				ChangeUrl('./attendance/AttendanceDetail.html?action=Edit&id='+$(this).attr('data'));
-			});
-		}, true);
+    	if(self.attendanceSelected()=="") return; 
+    	
+    	GetData();
     };
     
     	
     //初始化数据--如何调用self.reloadData
     self.reloadData();
+    
+    self.attendanceChage=function(p){
+    	self.reloadData();
+    };
 	
 	//搜索
 	self.search = function(obj) {
-		query.pageNo=1;
-		query.pageSize = 20;
-		qeury.keyWord = $("txtKeywords").val(); //查询参数格式
-		myAjax("/attendancedetails", "GET", query, doQueryActionSuccess, true);
+		GetData();
     };
-    
-    //新增
-    self.add = function(obj) {
-    	ChangeUrl('./attendance/AttendanceDetail.html?action=Add');
-    };
-    
+
     //修改
     self.modify=function(obj){
     	ChangeUrl('./attendance/AttendanceDetail.html?action=Edit&id='+obj.attId());
     };
-    
-    //删除
-    self.delete=function(obj){
-    	parent.dialog({
-            title: '提示',
-            content: '确定要删除该记录！',
-            okValue: '确定',
-            ok: function () {
-		    	var id = $(event.currentTarget).attr('data');
-		    	myAjax("/attendancedetail/"+id, "DELETE", null, reloadDate, false);
-			}
-        }).showModal();
-    }
-    
-    //批量删除
-    self.deletes = function(obj) {
-    	if ($(".checkall input:checked").size() < 1) {
-        	parent.dialog({
-	            title: '提示',
-	            content: '对不起，请选中您要操作的记录！',
-	            okValue: '确定',
-	            ok: function () { }
-	        }).showModal();
-	        return false;
-	    }
-	    var msg = "删除记录后不可恢复，您确定吗？";
-	    parent.dialog({
-	        title: '提示',
-	        content: msg,
-	        okValue: '确定',
-	        ok: function () {
-	        	$(".checkall input:checked").each(function(i){
-	        		myAjax("/attendancedetail/"+id, "DELETE", null, null, false);
-	        	});
-	        	location.reload();
-	        },
-	        cancelValue: '取消',
-	        cancel: function () { }
-	    }).showModal();
-    };
-
 };
 
 $().ready(function(){
 	viewMode = new AttendanceDetailViewModel();
     ko.applyBindings(viewMode);
+    
+    //获取下拉列表数据
+	myAjax("/attendances", "GET", null, function (data){
+		$.map(data.data, function(item) { attendanceList.push({id:item.attendanceId,name:item.attendanceName})});  
+		viewMode.attendanceList(attendanceList);
+		viewMode.attendanceSelected(attendanceList[0].name);
+		$(".rule-single-select").ruleSingleSelect();
+		
+		viewMode.reloadData();
+	}, true);
+	
+    //美化下拉框样式
+	$(".rule-single-select").ruleSingleSelect();
 });
 
-var bindPage =function(){
-	//分页控件加载处理
-    $.jqPaginator('#pagination', {
-        totalPages: myPage.totalPages,
-        visiblePages: myPage.limit,
-        currentPage: myPage.page,
-        onPageChange: function (num, type) {
-        	if(query.pageNo!=num){
-        		query.pageNo=num;
-        		viewMode.reloadData();
-        	}  
-//            if (type != 'init') {
-//            	ChangeUrl('./attendance/AttendanceDetailList.html?page=' + num);
-//            }
-        }
-    });
+function GetData(){
+	var searchKey = $("#txtKeywords").val().toString(); 
+	var url = "/attendance/"+self.attendanceSelected()+"/details";
+	
+	if(searchKey!="" && searchKey.indexOf("-")>0)
+		url=url+'?yearMonth='+searchKey;
+		
+	myAjax(url, "GET", null, function (data){
+		var mappedTasks = $.map(data, function(item) { return new Node(item) });  
+		self.attendanceDetailList(mappedTasks);
+		    
+		$("table tbody td .tomodify").bind(function(){
+			ChangeUrl('./attendance/AttendanceDetail.html?action=Edit&id='+$(this).attr('data'));
+		});
+	}, true);
 }
-
