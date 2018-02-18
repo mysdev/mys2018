ab606034fa2a16f2c6f8cfafc649b54acc46c1ea
Eddie008 <32446708+Eddie008@users.noreply.github.com>
2018-02-18 16:27:00 +0800

    Update EmployeeAttendList.js
    考勤页面修改

--- trunk/src/main/resources/static/pc/attendance/model/EmployeeAttendList.js
+++ trunk/src/main/resources/static/pc/attendance/model/EmployeeAttendList.js
@@ -1,42 +1,127 @@
+var myPage;
+var viewMode;
+var query = {};
+query.pageNo=1;
+query.pageSize = 20;
+if(getQueryString('page')!=undefined){
+	query.pageNo=getQueryString('page');
+}
+
+function Node(obj) {
+	this.attId = ko.observable(obj.attId); 
+	this.employeeId = ko.observable(obj.employeeId); 
+	this.attTime = ko.observable(obj.attTime); 
+	this.createdBy = ko.observable(obj.createdBy); 
+	this.createdDate = ko.observable(obj.createdDate); 
+	this.updatedBy = ko.observable(obj.updatedBy); 
+	this.updatedDate = ko.observable(obj.updatedDate); 
+}
+
+
 //定义ViewModel对象
-var EmployeeAttendanceEditViewModel = function () {  
-	var self=this;
-	self.linkId = ko.observable(''); 
-	self.empId = ko.observable(''); 
-	self.attendanceId = ko.observable(''); 
-
-    var opFalg=getQueryString('action');
+var EmployeeAttendViewModel = function () {  
+	self=this;
+    //添加动态监视数组对象
+    self.employeeAttendList = ko.observableArray([]);
     
-    if(opFalg!="Add"){
-    	var opid=getQueryString('id');
-    	myAjax("/employeeattendance/"+opid, "GET", null, function (data){
-			self.linkId(data.linkId);
-			self.empId(data.empId);
-			self.attendanceId(data.attendanceId);
+    //重载数据
+    self.reloadData = function(){
+    	myAjax("/employeeattends", "GET", query, function (data){
+			var mappedTasks = $.map(data.data, function(item) { return new Node(item) });  
+			self.employeeAttendList(mappedTasks);
+			myPage = data.page;
+			bindPage();
+			    
+			$("table tbody td .tomodify").bind(function(){
+				ChangeUrl('./attendance/EmployeeAttend.html?action=Edit&id='+$(this).attr('data'));
+			});
 		}, true);
-	}
-
-	//【提交】按钮押下处理
-    self.Commit = function () {    
-    	var submitPar ={};
-		submitPar.empId=self.empId();
-		submitPar.attendanceId=self.attendanceId();
-    	
-    	if(opFalg=="Add"){
-    		myAjaxJson("/employeeattendance", "POST", submitPar, function (data){
-				ChangeUrl("./attendance/EmployeeAttendanceList.html");
-			}, true);
-		}else{
-    		var opid=getQueryString('id');
-    		myAjaxJson("/employeeattendance/"+opid, "PUT", submitPar, function (data){
-				ChangeUrl("./attendance/EmployeeAttendanceList.html");
-			}, true);
-    	}
     };
+    
+    	
+    //初始化数据--如何调用self.reloadData
+    self.reloadData();
+	
+	//搜索
+	self.search = function(obj) {
+		query.pageNo=1;
+		query.pageSize = 20;
+		qeury.keyWord = $("txtKeywords").val(); //查询参数格式
+		myAjax("/employeeattends", "GET", query, doQueryActionSuccess, true);
+    };
+    
+    //新增
+    self.add = function(obj) {
+    	ChangeUrl('./attendance/EmployeeAttend.html?action=Add');
+    };
+    
+    //修改
+    self.modify=function(obj){
+    	ChangeUrl('./attendance/EmployeeAttend.html?action=Edit&id='+obj.attId());
+    };
+    
+    //删除
+    self.delete=function(obj){
+    	parent.dialog({
+            title: '提示',
+            content: '确定要删除该记录！',
+            okValue: '确定',
+            ok: function () {
+		    	var id = $(event.currentTarget).attr('data');
+		    	myAjax("/employeeattend/"+id, "DELETE", null, reloadDate, false);
+			}
+        }).showModal();
+    }
+    
+    //批量删除
+    self.deletes = function(obj) {
+    	if ($(".checkall input:checked").size() < 1) {
+        	parent.dialog({
+	            title: '提示',
+	            content: '对不起，请选中您要操作的记录！',
+	            okValue: '确定',
+	            ok: function () { }
+	        }).showModal();
+	        return false;
+	    }
+	    var msg = "删除记录后不可恢复，您确定吗？";
+	    parent.dialog({
+	        title: '提示',
+	        content: msg,
+	        okValue: '确定',
+	        ok: function () {
+	        	$(".checkall input:checked").each(function(i){
+	        		myAjax("/employeeattend/"+id, "DELETE", null, null, false);
+	        	});
+	        	location.reload();
+	        },
+	        cancelValue: '取消',
+	        cancel: function () { }
+	    }).showModal();
+    };
+
 };
 
-
 $().ready(function(){
-	$("#txtName").focus();
-    ko.applyBindings(new EmployeeAttendanceEditViewModel());
+	viewMode = new EmployeeAttendViewModel();
+    ko.applyBindings(viewMode);
 });
+
+var bindPage =function(){
+	//分页控件加载处理
+    $.jqPaginator('#pagination', {
+        totalPages: myPage.totalPages,
+        visiblePages: myPage.limit,
+        currentPage: myPage.page,
+        onPageChange: function (num, type) {
+        	if(query.pageNo!=num){
+        		query.pageNo=num;
+        		viewMode.reloadData();
+        	}  
+//            if (type != 'init') {
+//            	ChangeUrl('./attendance/EmployeeAttendList.html?page=' + num);
+//            }
+        }
+    });
+}
+
